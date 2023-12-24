d805bd6e3da1d1be716187e9a279d24e81e7c6bc
5up37 <augusto.silva.queiroz@gmail.com>
2023-12-24 09:59:12 -0300

    Add reset button and change color of grid

--- java.js
+++ java.js
@@ -1,24 +1,27 @@
 let container = document.querySelector('.container')
 container.style.cssText = 'display: flex; flex-wrap: wrap; height: 960px; width:960px'
-
+let body = document.querySelector('body')
 
 function createGrid(size) {
-for (let i = 1; i <= size*size; i++) {
+for (let i = 1; i <= size*size && size<=100; i++) {
     let div = document.createElement('div')
-    div.style.cssText = `width: ${960 / size}px; height: ${960 / size}px; flex: 0 0 auto`
-    container.appendChild(div)
-}}
-
-createGrid(10)
-
-let divs = document.querySelectorAll('.container div')
+    div.style.cssText = `width: ${960 / size}px; height: ${960 / size}px; flex: 0 0 auto; background-color: black`
+    container.appendChild(div)  
+}
+let divs = document.querySelectorAll('div div')
 divs.forEach((div) => {
     div.addEventListener('mouseenter',(e) => {
-        div.style.backgroundColor = 'green'
+        div.style.backgroundColor = 'white'
     })
-})
+})}
 
-let button = document.querySelector
-button.addEventListener('Click',(e) => {
-    container.removeChild(divs)
+createGrid(16)
+
+let button = document.querySelector('button')
+button.addEventListener('click',(e) => {
+    body.removeChild(container)
+    container = document.createElement('div')
+    container.style.cssText = 'display: flex; flex-wrap: wrap; height: 960px; width:960px'
+    body.insertBefore(container,button)
+    createGrid(prompt('Size of grid'))
     })
